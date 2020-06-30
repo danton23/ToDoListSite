8d58c8218d91823db35adeef62aabd3272f07294
Charlie <melvins_fan@hotmail.com>
2020-06-30 17:14:11 +0100

    Now View Tab takes you to the most recently added list by default

--- main/views.py
+++ main/views.py
@@ -26,13 +26,10 @@ def index(response,id):
                     ls.item_set.create(text=txt, complete=False)
                else:
                     print("invalid")
-               
-               
-               
+
      return render(response, "main/list.html",{"ls":ls, "items":items})
-def home(response):
-     ls=ToDoList.objects.all()
-     return render(response, "main/home.html",{"ls":ls})
+
+             
 def create(response):
             if response.method=="POST":
                  print("goober")
@@ -46,6 +43,41 @@ def create(response):
                            return render(response, "main/create.html",{"form":form}) 
             else:
                 form = CreateNewList()
-            return render(response, "main/create.html",{"form":form})  
+            return render(response, "main/create.html",{"form":form})               
+def view(response):
+     #try:
+               ident=ToDoList.objects.latest('id').id
+                  
+               ls=ToDoList.objects.get(id=ident)
+               items=ls.item_set.all()
+               #ls=ToDoList.objects.all()
+               if response.method=="POST":
+                    print(response.POST)
+                    if response.POST.get("save"):
+                         for item in ls.item_set.all():
+                              if response.POST.get("c"+str(item.id)) =="clicked":
+                                   item.complete=True
+                                   
+                              else:
+                                   item.complete=False
+                              item.save()     
+                                   
+                              
+
+                    elif response.POST.get("newItem"):
+                         txt=response.POST.get("new")
+                         if len(txt) >2:
+                              ls.item_set.create(text=txt, complete=False)
+                         else:
+                              print("invalid")               
+               return render(response, "main/list.html",{"ls":ls, "items":items})
+     #except:
+        #  print("not worked")
+         # create(response)
+          
+def home(response):
+     ls=ToDoList.objects.all()
+     return render(response, "main/home.html",{"ls":ls})
+
 
                 
